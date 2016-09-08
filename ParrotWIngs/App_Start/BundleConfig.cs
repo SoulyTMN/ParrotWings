9f48914a6f5756aa87e3f3dedf6c5b0aaa5e5864
Nikitin-SV <Nikitin-SV@zsb.sbrf.ru>
2016-09-08 18:12:52 +0500

    Parrot Wings version 1.02: font-awesome support added

--- ParrotWIngs/App_Start/BundleConfig.cs
+++ ParrotWIngs/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ParrotWIngs
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
                       "~/Content/angular-material.min.css",
-                      "~/Content/font-awesome.min.css",
+                      "~/Content/font-awesome-4.6.3/css/font-awesome.min.css",
                       "~/Content/jquery.mCustomScrollbar.min.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
